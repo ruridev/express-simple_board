98d37bcb852e4786df581f1b6934d6ff4075cf0e
ruridev <lee@sansan.com>
2020-03-12 18:17:28 +0900

    :pizza:

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
-
-var pool = pg.Pool({
-  database: 'board_development',
-  user: 'board',
-  password: 'boardboard',
-  host: 'database-board.cpw29ttz0zzl.ap-northeast-1.rds.amazonaws.com',
-  port: 5432,
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-   res.redirect('/posts');
+   res.status(302).redirect('/posts');
 });
 
 
