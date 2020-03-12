335d92bd2b66e980424d3437367b5d7923e4c10f
ruridev <lee@sansan.com>
2020-03-12 18:17:28 +0900

    :pizza:

--- routes/comments.js
+++ routes/comments.js
@@ -13,21 +13,58 @@ router.get('/:id/edit', function (req, res, next) {
         'SELECT id, body, writer, created_at, updated_at, post_id, status FROM post_comments WHERE id = $1',
         [req.params.id],
         function (err, result) {
-          console.log(err);
-          res.render('comments/edit', {
+          if (err) console.log(err);
+          res.status(200).render('comments/edit', {
             comment: result.rows[0]
           });
         },
       );
-      return "error";
+    }
+  });
+});
+
+/* GET post */
+router.post('/:id', function (req, res, next) {
+  db.connect(function (err, client) {
+    if (err) {
+console.log(err);
+return err;
+    } else {
+      client.query(
+        'update post_comments set body = $1, updated_at = $2 where id = $3 RETURNING *',
+        [req.body.body, new Date(), req.params.id],
+        function (err, result) {
+          if (err) console.log(err);
+          res.status(302).redirect('/posts/' + result.rows[0].post_id);
+        },
+      );
     }
   });
 });
 
 /* GET post */
 router.get('/:id/delete', function (req, res, next) {
-  res.render('comments/delete', {
-    post_id: req.params.id
+  res.status(200).render('comments/delete', {
+    comment_id: req.params.id
+  });
+});
+
+/* GET post */
+router.post('/:id/delete', function (req, res, next) {
+  db.connect(function (err, client) {
+    if (err) {
+console.log(err);
+return err;
+    } else {
+      client.query(
+        'UPDATE post_comments set status=1 where id = $1 RETURNING *',
+        [req.params.id],
+        function (err, result) {
+          if (err) console.log(err);
+          res.status(302).redirect('/posts/' + result.rows[0].post_id);
+        },
+      );
+    }
   });
 });
 
--- routes/posts.js
+++ routes/posts.js
@@ -3,85 +3,159 @@ var router = express.Router();
 var db = require('../models/database');
 const bodyParser = require('body-parser');
 
-router.use(bodyParser.urlencoded({
-  extended: true
-}));
+router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 /* GET posts listing. */
 router.get('/', function (req, res, next) {
   db.connect(function (err, client) {
     if (err) {
-      console.log(err);
-      return err;
+console.log(err);
+return err;
     } else {
       client.query(
-        'SELECT id, title, body, writer, hit_count, created_at, updated_at, parent_id, sort_key, status FROM posts',
+        'SELECT id, title, body, writer, hit_count, created_at, updated_at, parent_id, sort_key, status FROM posts order by sort_key, sort_key||id',
         function (err, result) {
-          console.log(err);
-          res.render('posts/list', {
+          if (err) console.log(err);
+          res.status(200).render('posts/list', {
             posts: result.rows,
           });
         },
       );
-      return "error";
-    }
-  });
-});
-
-/* GET posts listing. */
-router.post('/', function (req, res, next) {
-  db.connect(function (err, client) {
-    if (err) {
-      console.log(err);
-      return err;
-    } else {
-      client.query(
-        'Insert INTO posts(writer, title, body, encrypted_password, sort_key, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *',
-        [req.body.writer, req.body.title, req.body.body, req.body.password, 'sort_key', new Date(), new Date()],
-        function (err, result) {
-          console.log(err);
-          res.redirect('/posts/' + result.rows[0].id);
-        },
-      );
-      return "error";
     }
   });
 });
 
 /* GET post */
 router.get('/new', function (req, res, next) {
-  res.render('posts/new', {
+  res.status(200).render('posts/new', {
     parent_id: -1,
     parent_post: null,
   });
 });
 
 /* GET post */
-router.get('/:id/edit', function (req, res, next) {
+router.get('/:id', function (req, res, next) {
   db.connect(function (err, client) {
     if (err) {
-      console.log(err);
-      return err;
+console.log(err);
+return err;
     } else {
       client.query(
         'SELECT id, title, body, writer, hit_count, created_at, updated_at, parent_id, sort_key, status FROM posts WHERE id = $1',
         [req.params.id],
         function (err, result) {
-          console.log(err);
-          res.render('posts/edit', {
-            post: result.rows[0]
+          if (err) console.log(err)
+
+          client.query(
+            'SELECT id, post_id, body, writer, created_at, status FROM post_comments WHERE post_id = $1 order by created_at',
+            [req.params.id],
+            function (err2, result2) {
+              if (err2) console.log(err2)
+              res.status(200).render('posts/view', {
+                post: result.rows[0],
+                comments: result2.rows
+              });
+            },
+          );
+        },
+      );
+    }
+  });
+});
+
+
+/* GET posts listing. */
+router.post('/', function (req, res, next) {
+  db.connect(function (err, client) {
+    if (err) {
+console.log(err);
+return err;
+    } else {
+      const parent_id = req.body.parent_id != undefined ? req.body.parent_id : 0;
+      client.query(
+        "SELECT sort_key||LPAD(to_hex(id), 10, '0') as sort_key from posts where id = $1",
+        [parent_id],
+        function (err, result0) {
+          client.query(
+            "Insert INTO posts(writer, title, body, encrypted_password, sort_key, parent_id, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+            [req.body.writer, req.body.title, req.body.body, req.body.password, result0.rows.length > 0 ? result0.rows[0].sort_key : parent_id.toString(16).padStart(10, '0'), parent_id, new Date(), new Date()],
+            function (err, result) {
+              if (err) console.log(err);
+              res.status(302).redirect('posts/' + result.rows[0].id);
+            },
+          );
+        },
+      );
+    }
+  });
+});
+
+/* GET posts listing. */
+router.post('/:id', function (req, res, next) {
+  db.connect(function (err, client) {
+    if (err) {
+console.log(err);
+return err;
+    } else {
+      client.query(
+        'update posts set writer = $1, title = $2, body = $3, updated_at = $4 where id = $5 RETURNING *',
+        [req.body.writer, req.breq.body.body, new Date(), req.params.id],
+        function (err, result) {
+          if (err) console.log(err);
+          res.status(302).redirect('/posts/' + result[0].id);
+        },
+      );
+    }
+  });
+});
+
+/* GET posts listing. */
+router.post('/:id/delete', function (req, res, next) {
+  db.connect(function (err, client) {
+    if (err) {
+console.log(err);
+return err;
+    } else {
+      if (err) console.log(err)
+      client.query(
+        'update posts set status = 1 where id = $1',
+        [req.params.id],
+        function (err, result) {
+          if (err) console.log(err);
+          res.status(302).redirect('/posts');
+        },
+      );
+    }
+  });
+});
+
+
+/* GET post */
+router.get('/:id/edit', function (req, res, next) {
+  db.connect(function (err, client) {
+    if (err) {
+      console.log(err);
+      res.status(302).redirect('/');
+    } else {
+      client.query(
+        'SELECT id, title, body, writer, hit_count, created_at, updated_at, parent_id, sort_key, status FROM posts WHERE id = $1',
+        [req.params.id],
+        function (err, result) {
+          if (err) console.log(err);
+          res.status(200).render('posts/edit', {
+            post: result.rows[0],
+            parent_post: null
           });
         },
       );
-      return "error";
     }
   });
 });
 
 /* GET post */
 router.get('/:id/delete', function (req, res, next) {
-  res.render('posts/delete', {
+  res.status(200).render('posts/delete', {
     post_id: req.params.id
   });
 });
@@ -91,67 +165,36 @@ router.get('/:id/reply', function (req, res, next) {
   db.connect(function (err, client) {
     if (err) {
       console.log(err);
-      return err;
+      res.status(302).redirect('/');
     } else {
       client.query(
         'SELECT id, title, body, writer, hit_count, created_at, updated_at, parent_id, sort_key, status FROM posts WHERE id = $1',
         [req.params.id],
         function (err, result) {
-          console.log(err);
-          res.render('posts/new', {
+          if (err) console.log(err)
+          res.status(200).render('posts/new', {
             parent_post: result.rows[0],
           });
         },
       );
-      return "error";
     }
   });
 });
 
-/* GET post */
-router.get('/:id', function (req, res, next) {
-  db.connect(function (err, client) {
-    if (err) {
-      console.log(err);
-      return err;
-    } else {
-      client.query(
-        'SELECT id, title, body, writer, hit_count, created_at, updated_at, parent_id, sort_key, status FROM posts WHERE id = $1',
-        [req.params.id],
-        function (err, result) {
-          console.log(err);
-
-          client.query(
-            'SELECT id, post_id, body, writer, created_at, status FROM post_comments WHERE post_id = $1 order by created_at',
-            [req.params.id],
-            function (err2, result2) {
-              console.log(err2);
-              res.render('posts/view', {
-                post: result.rows[0],
-                comments: result2.rows
-              });
-            },
-          );
-        },
-      );
-      return "error";
-    }
-  });
-});
 
 /* GET post */
 router.post('/:post_id/comments', function (req, res, next) {
   db.connect(function (err, client) {
     if (err) {
       console.log(err);
-      return err;
+      res.status(302).redirect('/');
     } else {
       client.query(
         'Insert INTO post_comments(post_id, writer, body, encrypted_password, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6)',
         [req.params.post_id, req.body.writer, req.body.body, req.body.password, new Date(), new Date()],
         function (err, result) {
-          console.log(err);
-          res.redirect('/posts/' + req.params.post_id);
+          if (err) console.log(err)
+          res.status(302).redirect('/posts/' + req.params.post_id);
         },
       );
     }
