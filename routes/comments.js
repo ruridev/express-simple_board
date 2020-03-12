43beb31e879ef2c8f55dc84a1ff6569719bc5439
ruri <devruri@gmail.com>
2020-03-13 00:06:43 +0900

    validation check

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models/database');
+const { check, validationResult } = require('express-validator/check');
 
 /* GET post */
 router.get('/:id/edit', function(req, res, next) {
@@ -28,27 +29,48 @@ router.get('/:id/edit', function(req, res, next) {
 });
 
 /* GET post */
-router.post('/:id', function(req, res, next) {
-  db.connect(function(err, client) {
-    try {
-      if (err) {
-        console.log(err);
-      } else {
-        client.query(
-          'update post_comments set body = $1, updated_at = $2 where id = $3 RETURNING *',
-          [req.body.body, new Date(), req.params.id],
-          function(err, result) {
-            if (err) console.log(err);
-            res.status(302).redirect('/posts/' + result.rows[0].post_id);
-          },
-        );
-      }
-    } finally {
-      console.log('커넥션 풀 반환');
-      client.release();
+router.post(
+  '/:id',
+  [
+    check('body')
+      .trim()
+      .isLength({ min: 1 })
+      .isString()
+      .exists(),
+    check('password')
+      .trim()
+      .isLength({ min: 1 })
+      .isString()
+      .exists(),
+    check('post_id').isNumeric(),
+  ],
+  function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors.array());
+      return res.render('422');
     }
-  });
-});
+    db.connect(function(err, client) {
+      try {
+        if (err) {
+          console.log(err);
+        } else {
+          client.query(
+            'update post_comments set body = $1, updated_at = $2 where id = $3 RETURNING *',
+            [req.body.body, new Date(), req.params.id],
+            function(err, result) {
+              if (err) console.log(err);
+              res.status(302).redirect('/posts/' + result.rows[0].post_id);
+            },
+          );
+        }
+      } finally {
+        console.log('커넥션 풀 반환');
+        client.release();
+      }
+    });
+  },
+);
 
 /* GET post */
 router.get('/:id/delete', function(req, res, next) {
