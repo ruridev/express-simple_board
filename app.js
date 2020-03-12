3d6ff0cd14e8041eface88a2325e0f5a25f64acd
ruri <devruri@gmail.com>
2020-03-12 23:32:36 +0900

    해당 루트가 없을 경우 404

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.use(bodyParser.json());
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/comments', commentsRouter);
+app.get('*', function(req, res) {
+  res.render('404');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
