6b01c3606ed6d7a647c44cbd9c5b8695fe216b01
ruri <devruri@gmail.com>
2020-03-12 23:10:01 +0900

    :sun: 커넥션 풀 닫기 :shirt: 디자인 정리 :wrench: 버그 수정

--- routes/comments.js
+++ routes/comments.js
@@ -3,68 +3,68 @@ var router = express.Router();
 var db = require('../models/database');
 
 /* GET post */
-router.get('/:id/edit', function (req, res, next) {
-  db.connect(function (err, client) {
+router.get('/:id/edit', function(req, res, next) {
+  db.connect(function(err, client) {
     if (err) {
       console.log(err);
-      return err;
     } else {
       client.query(
-        'SELECT id, body, writer, created_at, updated_at, post_id, status FROM post_comments WHERE id = $1',
+        "SELECT id, body, writer, to_char(created_at, 'yyyy/mm/dd hh24:mm:ss') as created_at , updated_at, post_id, status FROM post_comments WHERE id = $1",
         [req.params.id],
-        function (err, result) {
+        function(err, result) {
           if (err) console.log(err);
           res.status(200).render('comments/edit', {
-            comment: result.rows[0]
+            comment: result.rows[0],
           });
         },
       );
     }
+    client.release();
   });
 });
 
 /* GET post */
-router.post('/:id', function (req, res, next) {
-  db.connect(function (err, client) {
+router.post('/:id', function(req, res, next) {
+  db.connect(function(err, client) {
     if (err) {
-console.log(err);
-return err;
+      console.log(err);
     } else {
       client.query(
         'update post_comments set body = $1, updated_at = $2 where id = $3 RETURNING *',
         [req.body.body, new Date(), req.params.id],
-        function (err, result) {
+        function(err, result) {
           if (err) console.log(err);
           res.status(302).redirect('/posts/' + result.rows[0].post_id);
         },
       );
     }
+    client.release();
   });
 });
 
 /* GET post */
-router.get('/:id/delete', function (req, res, next) {
+router.get('/:id/delete', function(req, res, next) {
   res.status(200).render('comments/delete', {
-    comment_id: req.params.id
+    comment_id: req.params.id,
   });
 });
 
 /* GET post */
-router.post('/:id/delete', function (req, res, next) {
-  db.connect(function (err, client) {
+router.post('/:id/delete', function(req, res, next) {
+  db.connect(function(err, client) {
     if (err) {
-console.log(err);
-return err;
+      console.log(err);
     } else {
       client.query(
-        'UPDATE post_comments set status=1 where id = $1 RETURNING *',
-        [req.params.id],
-        function (err, result) {
+        'UPDATE post_comments set status=1, updated_at = $2 where id = $1 RETURNING *',
+        [req.params.id, new Date()],
+        function(err, result) {
           if (err) console.log(err);
           res.status(302).redirect('/posts/' + result.rows[0].post_id);
         },
       );
     }
+    client.release();
   });
 });
 
