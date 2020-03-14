93da774a3c15663e4570e478f0600e2b075820fe
ruri <devruri@gmail.com>
2020-03-14 17:16:59 +0900

    :disk: 첨부파일 등록

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -19,23 +20,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.json({
-  type: 'application/*+json'
-}));
+app.use(
+  bodyParser.json({
+    type: 'application/*+json',
+  }),
+);
 
-// parse application/x-www-form-urlencoded 
-app.use(bodyParser.urlencoded({
-  extended: false,
-  type: 'application/x-www-form-urlencoded'
-}));
+// parse application/x-www-form-urlencoded
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+    type: 'application/x-www-form-urlencoded',
+  }),
+);
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/comments', commentsRouter);
-app.get('*', function(req, res) {
-  res.render('404');
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -51,6 +54,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.get('*', function(req, res) {
+  res.render('404');
 });
 
 module.exports = app;
